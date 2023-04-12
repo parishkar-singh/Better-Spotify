c75e2d7577d0256a9c050af73297d357c6278362
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-04-12 22:09:22 +0530

    feat: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,39 @@
-import {useState} from "react"
-import {NavLink} from "react-router-dom";
-import {RiCloseLine} from "react-icons/all";
-import {logo} from "../assets"
-import {links } from "../assets/constants"
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineMenu, RiCloseLine } from 'react-icons/all';
+import logo from '../assets/logo2.png';
+import { links } from '../assets/constants';
 
+const NavLinks = ({handleClick}) => (
+  <div className="mt-10">
+    {links.map((item) => (
 
-const Sidebar = () => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        onClick={() =>handleClick&&handleClick()}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-purple-400">
+        <item.icon className="w-6 h-6 mr-2"/>
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+)
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  return (
     <>
-        <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]" >
-            <img className='w-full h-14 object-contain' src={logo} alt="logo"/>
-        </div>
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <img className="w-full h-14 object-contain rounded-3xl h-52 w-auto" src={logo} alt="logo"/>
+      <NavLinks/>
+    </div>
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen?(
+          <RiCloseLine className='w-6 h-6 text-white mr-2'/>
+        ):(<HiOutlineMenu className='w-6 h-6 text-white mr-2' />)}
+      </div>
     </>
-);
+  );
+};
 
 export default Sidebar;
