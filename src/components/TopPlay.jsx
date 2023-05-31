c86a8e7e64d83da6d2e0836b9f0b27582e929ce4
Parishkar Singh <79470399+parishkar-9790@users.noreply.github.com>
2023-05-31 14:04:52 +0530

    feat : added top play

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import 'swiper/css'
-import 'swiper/css/free-mode'
-import {Link} from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
-import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // import {freeMode, navigation, pagination} from 'swiper';
 import PlayPause from './PlayPause';
-import {playPause,setActiveSong} from '../redux/features/playerSlice';
-import data from '../database/getTopCharts.json'
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import data from '../database/getTopCharts.json';
+
 const TopPlay = () => {
   const dispatch = useDispatch();
-  const {activeSong,isPlaying} = useSelector(state => state.player)
+  const {
+    activeSong,
+    isPlaying
+  } = useSelector(state => state.player);
   const handlePauseClick = () => {
 
-    dispatch(playPause(false))
-  }
+    dispatch(playPause(false));
+  };
   const handlePlayClick = () => {
-    dispatch(setActiveSong({song,data,i}))
-    dispatch(playPause(true))
-  }
-return(
+    dispatch(setActiveSong({
+      song,
+      data,
+      i
+    }));
+    dispatch(playPause(true));
+  };
+  return (
+    <div>
+      TopPlay
+    </div>
 
-
-  <div>TopPlay</div>
-  )
-}
+  );
+};
 
 export default TopPlay;
